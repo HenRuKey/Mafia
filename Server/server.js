a2ad0c0f675980cb874055ed6c453c357ac51b5b
Unknown <rob12057@gmail.com>
2018-11-28 15:58:51 -0700

    Auto stash before merge of "master" and "origin/master"

--- Server/server.js
+++ Server/server.js
@@ -4,28 +4,106 @@ var app = express();
 var http = require('http').Server(app);
 var mongojs = require('mongojs');
 var db = mongojs('mongodb://bryan:mafiadb1@ds029267.mlab.com:29267/game_info', ['rooms']);
+var bodyParser = require('body-parser');
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/api", (req, res) =>{
-    db.rooms.find(function(err, rooms){
-        if(err){
-            console.log("Thrown Here")
+
+//Returns all rooms
+app.get("/api", (req, res) => {
+    db.rooms.find(function (err, rooms) {
+        if (err) {
+            console.log(err)
             res.send(err)
         }
         res.json(rooms);
     });
 })
 
-app.get("/api/:code", (req, res) =>{
-    var query = {room_id: req.params.code}
-    db.rooms.findOne(query, function(err, room){
-        if(err){
-            console.log("Thrown Here")
-            res.send(err)   
+//Returns the room matching the code
+app.get("/api/:code", (req, res) => {
+    var query = { room_id: req.params.code }
+    db.rooms.findOne(query, function (err, room) {
+        if (err) {
+            console.log(err)
+            res.send(err)
         }
         res.json(room);
     });
+})
+
+
+//Creates a room with a given code.
+app.post("/api/create/:code", (req, res) => {
+    var room = req.body;
+    var id = room.room_id;
+    if (!id) {
+        res.status(400);
+        res.json("Invalid Data");
+    } else {
+        db.rooms.save(room, function (err, room) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(room);
+        })
+    }
+})
+
+//Updates the room with the matching code. Deletes the room if empty;
+app.put("/api/update/:code", (req, res) => {
+    var id = req.params.code;
+    var newOccupants = req.body.occupants;
+    var query = { room_id: id }
+    console.log("Runs");
+    if (!id) {
+        res.status(400);
+        res.json("Invalid Data")
+    } else {
+        db.rooms.update(query, {$inc: {occupants: newOccupants}}, function (err, response) {
+            if (err) {
+                res.send(err);
+                console.log(err)
+            }
+            console.log("Updates");
+            db.rooms.findOne(query, function (err, room) {
+                if (err) {
+                    console.log(err)
+                    res.send(err)
+                } if (room["occupants"] == 0) {
+                    db.rooms.remove({ room_id: id }, function (err, room) {
+                        if (err) {
+                            res.send(err);
+                        }
+                        res.json("Deleted");
+                        console.log("Deletes");
+                    })
+                } else {
+                    res.json(room);
+                }   
+            });
+        })
+    }
+})
+
+
+
+// Delete the room with the matching code
+app.delete("/api/deleteRoom/:code", (req, res) => {
+    var id = req.params.code;
+    if (!id) {
+        res.status(400);
+        res.json("Invalid Data")
+    } else {
+        db.rooms.remove({ room_id: id }, function (err, room) {
+            if (err) {
+                res.send(err);
+            }
+            res.json("Deleted");
+        })
+    }
 })
 
 
--- src/app/room-manager/room-manager.component.ts
+++ src/app/room-manager/room-manager.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import * as $ from 'jquery';
-import {MafiaDbService} from '../mafia-db.service';
+import { MafiaDbService } from '../mafia-db.service';
+import { getRandomString } from 'selenium-webdriver/safari';
 
 @Component({
   selector: 'app-room-manager',
@@ -12,9 +13,17 @@ import {MafiaDbService} from '../mafia-db.service';
 
 export class RoomManagerComponent implements OnInit {
 
-  tests = [];
+
+
+/**
+ * Temporary test variable used for tracking the room code of a created or joined room.
+ * TODO: Move this to whichever component.ts will be handling specific room logic
+ */
+  room = "" 
+
+
+
   constructor(private service: MafiaDbService) {
-    this.tests = service.checkRoomByID('ROOM');
   }
 
   ngOnInit() {
@@ -42,7 +51,28 @@ export class RoomManagerComponent implements OnInit {
    * Initializes a room with a unique four-letter key.
    */
   createRoom() {
-    return false;
+    var code = this.generateRoomCode();
+    this.service.CheckRoomByID(code, result => {
+      if (result == null) {
+        this.service.CreateRoom(code, succeed => {
+          
+
+          //TODO: Redirect to proper room, passing along room code
+
+
+          this.room = code;
+          console.log(succeed)
+        },
+          fail => {
+            console.log(fail)
+          })
+
+      } else {
+        console.log("Room already exists. Creating new one...")
+        //TODO Handle the rare case of a room already existing.
+      }
+    })
+
   }
 
   /**
@@ -57,15 +87,62 @@ export class RoomManagerComponent implements OnInit {
    * @param roomCode  the room to join
    */
   joinRoom(roomCode: string) {
+    roomCode = roomCode.toUpperCase();
     if (this.isValidRoomCode(roomCode)) {
-      this.service.checkRoomByID(roomCode)
       $(".error").css("visibility", "hidden");
+      this.service.CheckRoomByID(roomCode, result => {
+        if (result != null) {
+          this.service.UpdateRoom(roomCode, 1, result => {
+
+            //TODO: Redirect to proper room, passing along the room code
+
+            this.room = roomCode;
+            console.log(result);
+
+          })
+        } else {
+          $(".error").text("Room Doesn't Exist")
+          $(".error").css("visibility", "visible");
+        }
+      })
     }
     else {
+      $(".error").text("Invalid Room Code")
       $(".error").css("visibility", "visible");
     }
     return false;
   }
+
+
+  /**
+   * Asks the service to delete a room
+   * NOTE: MANUAL OVERRIDES ONLY - see mafia-db.service.ts for details.
+   * @param roomCode room to delete
+   */
+  deleteRoom(roomCode: string) {
+    if (this.isValidRoomCode(roomCode)) {
+      this.service.DeleteRoom(roomCode, result => {
+        console.log(result);
+      })
+    }
+  }
+
+  /**
+   * Handles event when client exist browser, redirects, submits form, etc.
+   * Asks service to update the server. Intended as a method of tracking when 
+   * a client leaves a room
+   * 
+   * 
+   * TODO: Move this method so it exists in the "room's" copmonent.ts
+   */
+  beforeExit() {
+    if (this.room != "") {
+      this.service.UpdateRoom(this.room, -1, result => {
+        console.log(result)
+      })
+    }
+  }
+
 
   /**
    * Validates a roomcode.
@@ -76,5 +153,18 @@ export class RoomManagerComponent implements OnInit {
     roomCode = roomCode.toUpperCase();
     return roomCode.length == 4;
   }
+
+  /**
+   * Generates a random 4 char room code in all caps.
+   * @returns the 4 char string
+   */
+  generateRoomCode(): string {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for (var i = 0; i < 4; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+  };
 
 }
