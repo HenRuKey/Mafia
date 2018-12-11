3598020f260d5d114f94724f1bc5fe5b842f7f59
Michael Zopff <michael.zopff@gmail.com>
2018-12-10 18:42:28 -0700

    Automatically scroll messageBox

--- src/app/message-box/message-box.component.ts
+++ src/app/message-box/message-box.component.ts
@@ -6,12 +6,20 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./message-box.component.css']
 })
 export class MessageBoxComponent implements OnInit {
-
+  
+  private mutationObserver;
+  private messageBox;
+  
   constructor() { }
 
   @Input() messages;
   
   ngOnInit() {
+    this.messageBox = document.getElementById("message-box");
+    this.mutationObserver = new MutationObserver(() => {
+      this.messageBox.scrollTop = this.messageBox.scrollHeight - this.messageBox.clientHeight;
+    });
+    this.mutationObserver.observe(this.messageBox, { attributes: false, childList: true, subtree: false });    
   }
 
 }
